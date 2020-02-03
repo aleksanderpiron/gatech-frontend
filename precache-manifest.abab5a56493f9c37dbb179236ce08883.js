0fdb09128270018f9c051a226d5e89c4de1065a2
Aleksander Piron <42944811+aleksanderpiron@users.noreply.github.com>
2020-02-03 19:58:55 +0100

    Updates

---
+++ precache-manifest.abab5a56493f9c37dbb179236ce08883.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8718de5b8a23d421068ee3d5436ed52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9b8f1d6066e270bcd9e",
+    "url": "/static/css/main.08d9ca1a.chunk.css"
+  },
+  {
+    "revision": "97d08923ebb26423425c",
+    "url": "/static/js/2.84453ae5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.84453ae5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d9b8f1d6066e270bcd9e",
+    "url": "/static/js/main.00771001.chunk.js"
+  },
+  {
+    "revision": "351239a5e725d6dd0cd2",
+    "url": "/static/js/runtime-main.bb80eec7.js"
+  },
+  {
+    "revision": "9b2528e28c93510f2cc86d9298934dbd",
+    "url": "/static/media/arrow.9b2528e2.svg"
+  },
+  {
+    "revision": "c358ddc965c18a9f3ed169daeabbc45c",
+    "url": "/static/media/close.c358ddc9.svg"
+  },
+  {
+    "revision": "15c631697775bfb40a87b622646ee189",
+    "url": "/static/media/search.15c63169.svg"
+  }
+]);
